1f7dc39abf148676468c54414214c15e0c50e3ac
Keoni Murray <keonimurray@Keonis-MacBook-Pro.local>
2019-10-14 18:38:30 -0700

    connected application to db

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const compress = require('compression')
 const index = require('./routes/index')
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(compress())
+
+require('dotenv').config()
+require('./config/db')
 
 app.get('/', index.serverActive)
 
---
+++ config/db.js
@@ -0,0 +1,26 @@
+
+const mongoose = require('mongoose')
+const assert = require('assert')
+
+const url = process.env.db_url
+
+mongoose.Promise = global.Promise
+mongoose.connect(
+  url,
+  {
+    useNewUrlParser: true
+  },
+  (err, db) => {
+    assert.strictEqual(null, err)
+    console.log('Connected successfully to database')
+
+    // db.close(); turn on for testing
+  }
+)
+mongoose.connection.on(
+  'error',
+  console.error.bind(console, 'MongoDB connection Error:')
+)
+mongoose.set('debug', true)
+
+module.exports = mongoose.connection
