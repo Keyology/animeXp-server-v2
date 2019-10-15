2e4bea08245216eb6bf94532d53b3808ef37a6d6
Keoni Murray <keonimurray@Keonis-MacBook-Pro.local>
2019-10-15 00:47:41 -0700

    added routes folders

---
+++ routes/search/search.js
@@ -0,0 +1,27 @@
+
+const checkQueryTypeString = require('./helper')
+
+exports.searchDbForAnime = async function (req, res) {
+  console.log('---QUERY----', req.params.query)
+  
+  return checkQueryTypeString.checkIfQueryIsAnimeId(req.params.query) ? console.log('---HIT---') : console.log("---NOT HIT------")
+
+
+    // validate query is string 
+    //check if string contains numbers of a certain length 
+    //create a helper function for search that searches the db for the anime
+    // if the db returs null call another helper function that scrapes data from kitsu
+
+    // and parses / process data
+
+// save data to db and return the results
+
+
+
+
+// validate query 
+// check if string contains numbers and is a certain length  to determine it's an id
+
+
+
+}
