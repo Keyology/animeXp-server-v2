6a7fa4ca3be8b68e471085d00f1168c754f131c2
Keoni Murray <keonimurray@Keonis-MacBook-Pro.local>
2019-10-14 23:26:36 -0700

    started search route

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const compress = require('compression')
 // importing routes
 const homeRoute = require('./routes/home')
 const createListRoute = require('./routes/createList/createList')
+const searchAnimeRoute = require('./routes/search/search')
 
 // importting middleware and functions
 const validateList = require('./middleware/validateListInput')
+const queryTypeString = require('./routes/search/middleware')
 
 // intializing express
 const app = express()
@@ -23,5 +25,6 @@ require('./config/db')
 // endpoints
 app.get('/', homeRoute.home)
 app.post('/api/v0/create/new/list', validateList.validateListInput, createListRoute.createList)
+app.get('/api/v0/search/:query', queryTypeString.checkIfQueryIsString, searchAnimeRoute.searchDbForAnime)
 
 module.exports = app
