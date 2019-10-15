cdebd4ed1bf2ee497373433f654694985d947f63
Keoni Murray <keonimurray@Keonis-MacBook-Pro.local>
2019-10-14 20:58:10 -0700

    create  validate list middle ware

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require('express')
 const compress = require('compression')
-const index = require('./routes/index')
+
+// importing routes
+const homeRoute = require('./routes/home')
+const createListRoute = require('./routes/createList/createList')
+
+// importting middleware and functions
+const validateList = require('./middleware/validateListInput')
+
+// intializing express
 const app = express()
 
+// middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(compress())
 
+// db setup
 require('dotenv').config()
 require('./config/db')
 
-app.get('/', index.serverActive)
+// endpoints
+app.get('/', homeRoute.home)
+app.post('/api/v0/create/new/list', validateList.validateListInput, createListRoute.createList)
 
 module.exports = app
---
+++ middleware/validateListInput.js
@@ -0,0 +1,20 @@
+const check = require('check-types')
+const validator = require('validator')
+
+exports.validateListInput = async function (req, res, next) {
+  const body = [req.body.list, req.body.name, req.body.description]
+  // Check input type
+
+  const validateListIsTypeArray = check.array(body[0])
+  const validateNameIsTypeString = check.string(body[1])
+  const validateDescriptionIsTypeString = check.string(body[2])
+
+  // check if list has a name that has only letters
+  const validateNameHasLetters = validator.isAlpha(body[1])
+
+  // check if input is valid
+  if (validateListIsTypeArray && validateNameHasLetters && validateNameIsTypeString && validateDescriptionIsTypeString) {
+    return next()
+  }
+  return res.status(412).send({ message: 'invalid input' })
+}
