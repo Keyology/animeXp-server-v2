28a9682d4cb3e5a0196e643879cd459a69358fcd
Keoni Murray <keonimurray@Keonis-MacBook-Pro.local>
2019-10-16 18:06:48 -0700

    built search route

--- jobs/tasks/searchJobs/helper.js
+++
@@ -1,7 +0,0 @@
-// const agendaFunctions = require('..//..//agenda')
-// const agenda = agendaFunctions.agenda
-
-// exports.startSaveToDbJob = (respBody) => {
-//     agenda.now('save anime from kitsu to db', { data: respBody })
-//     return agenda.start('save anime from kitsu to db')
-// }
--- routes/search/search.js
+++ routes/search/search.js
@@ -14,9 +14,6 @@ exports.searchForAnime = async function (req, res) {
       const checkForUpdateSearchResults = await queryDb.searchAnimeByName(searchQuery)
       return res.status(200).json(checkForUpdateSearchResults)
     }
-    // search for anime by id if not null return results otherwise call kitsu to get anime  
-   // if anime id is 24 charcters don't pass to kitsu api call return could not find anime 
-   // otherwise save kitsu response to db and return 
   } catch (error) {
     res.status(412).send({ message: 'Invalid input' })
     console.error(`COULD NOT FIND QUERY: ${searchQuery}`)
